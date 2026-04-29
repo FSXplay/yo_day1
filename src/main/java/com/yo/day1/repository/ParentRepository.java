d2828f79e5410bbe5664daba0c3090dbc1d82c7c
FSXplay <skinoftheduck@gmail.com>
2026-04-29 15:05:51 +0800

    Use NativeQuery

--- src/main/java/com/yo/day1/repository/ParentRepository.java
+++ src/main/java/com/yo/day1/repository/ParentRepository.java
@@ -1,13 +1,12 @@
 package com.yo.day1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 import com.yo.day1.domain.entity.Parent;
 
 public interface ParentRepository extends JpaRepository<Parent, Long> {
-    @Query(value = "SELECT (EXISTS (SELECT 1 FROM students s WHERE s.parent_id = :id) " +
-                   "OR EXISTS (SELECT 1 FROM users u WHERE u.parent_id = :id))", nativeQuery = true)
-    boolean existsLinkedEntities(@Param("id") Long id);
+    @NativeQuery(value = "SELECT (EXISTS (SELECT 1 FROM students s WHERE s.parent_id = :id) " +
+                   "OR EXISTS (SELECT 1 FROM users u WHERE u.parent_id = :id))")
+    boolean existsLinkedEntities(Long id);
 }
