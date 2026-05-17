12b106860f5cb2e2501270e6268da3b0c0565162
FSXplay <skinoftheduck@gmail.com>
2026-05-17 15:44:28 +0800

    Implement auth

---
+++ src/main/java/com/yo/day1/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.yo.day1.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setSkipNullEnabled(true)
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMethodAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PUBLIC);
+        return modelMapper;
+    }
+}
